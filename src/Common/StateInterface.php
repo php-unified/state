1d441751057261d6a2e463d307c6dec48ddbb80a
Marcel Frankruijter <8653925+mfrankruijter@users.noreply.github.com>
2019-02-21 19:30:14 +0100

    Refactor: package maintenance
    Added CHANGELOG.md
    Added CONTRIBUTING.md
    Added PHPCS package and configuration
    Added rules to archive exclusion of composer.json
    Added composer cache and phpcs test step to travis build
    Added contributing and change log additions to readme
    Fixed some docblocks and suppressed warnings

--- src/Common/StateInterface.php
+++ src/Common/StateInterface.php
@@ -61,6 +61,8 @@ interface StateInterface
      * Adds a state to the state.
      *
      * @param StateInterface $state The state that should be a sub-state of this state.
+     *
+     * @return void
      */
     public function addState(StateInterface $state): void;
 }
